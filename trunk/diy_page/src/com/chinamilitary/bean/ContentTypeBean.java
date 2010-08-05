c4a76e3aeeba60ac8db5801096a183ada9649f2c
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2010-08-05 11:55:40 +0000

    类型对象

---
+++ trunk/diy_page/src/com/chinamilitary/bean/ContentTypeBean.java
@@ -0,0 +1,37 @@
+package com.chinamilitary.bean;
+
+public class ContentTypeBean {
+
+	private String ext;
+	private String contentType;
+	private String subMissions ;
+	
+	public ContentTypeBean(){
+	}
+
+	public String getContentType() {
+		return contentType;
+	}
+
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+
+	public String getExt() {
+		return ext;
+	}
+
+	public void setExt(String ext) {
+		this.ext = ext;
+	}
+
+	public String getSubMissions() {
+		return subMissions;
+	}
+
+	public void setSubMissions(String subMissions) {
+		this.subMissions = subMissions;
+	}
+	
+	
+}
