eb3f3adaa2413ef61977596e89cbcfe54c52dab2
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2010-08-10 15:42:58 +0000

    修改获取链接方法的参数

--- diy_page/src/com/chinamilitary/htmlparser/AbstractParser.java
+++ diy_page/src/com/chinamilitary/htmlparser/AbstractParser.java
@@ -4,14 +4,16 @@ import java.util.HashMap;
 
 import com.chinamilitary.bean.LinkBean;
 
-public abstract class AbstractParser {
+public interface AbstractParser {
 
-	public abstract HashMap<String,LinkBean> getLink(String url) throws Exception;
+	public abstract HashMap<String,LinkBean> getLink(String url,String encoding) throws Exception;
 	
-	public abstract HashMap<String,LinkBean> getLink(String url,String attribute,String value) throws Exception;
+	public abstract HashMap<String,LinkBean> getLinkByTag(String url,String attribute,String value) throws Exception;
 	
-	public abstract HashMap<String,LinkBean> getDiv(String url) throws Exception;
+	public abstract HashMap<String,LinkBean> getLinkByTag(String url,String attribute,Class cls,String value) throws Exception;
 	
-	public abstract HashMap<String,LinkBean> getDiv(String url,String attribute,String value) throws Exception;
+	public abstract HashMap<String,LinkBean> getDiv(String url,String encoding) throws Exception;
+	
+	public abstract HashMap<String,LinkBean> getByTag(String url,Class cls,String attribute,String value);
 	
 }
