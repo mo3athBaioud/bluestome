1964680e79b0f6746f1223aab06cc8b3ec3ae661
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2010-09-10 09:39:45 +0000

    增加测试类

---
+++ trunk/diy_page/test/com/dao/PicFileDAOTest.java
@@ -0,0 +1,88 @@
+package com.dao;
+
+import java.io.File;
+import java.util.List;
+
+import org.junit.*;
+
+import com.chinamilitary.bean.PicfileBean;
+import com.chinamilitary.bean.WebsiteBean;
+import com.chinamilitary.dao.PicFileDao;
+import com.chinamilitary.dao.WebSiteDao;
+import com.chinamilitary.factory.DAOFactory;
+import com.chinamilitary.util.StringUtils;
+import com.utils.FileUtils;
+
+public class PicFileDAOTest {
+
+	PicFileDao picFiledao = null;
+	WebSiteDao webSiteDao = null;
+	List<PicfileBean> list = null;
+	List<WebsiteBean> webList = null;
+	final static String FILE_SERVER = "D:\\fileserver\\image\\";
+	@Before
+	public void init(){
+		picFiledao = DAOFactory.getInstance().getPicFileDao();
+		webSiteDao = DAOFactory.getInstance().getWebSiteDao();
+	}
+	
+	@After
+	public void destory(){
+		if(null != picFiledao)
+			picFiledao = null;
+		if(null != webSiteDao)
+			webSiteDao = null;
+		if(null != list)
+			list.clear();
+		if(null != webList)
+			webList.clear();
+		System.gc();
+		System.out.println("gc");
+	}
+	
+	public void find() throws Exception{
+		list = picFiledao.findAll();
+		webList = webSiteDao.findByParentId(600);
+		Assert.assertNotNull(list);
+		System.out.println("总数:"+list.size());
+	}
+	
+	public void findByid() throws Exception{
+		
+	}
+	
+	@Test
+	public void update() throws Exception{
+		PicfileBean bean = picFiledao.findById(284706);
+		Assert.assertNotNull(bean);
+		System.out.println("name:"+bean.getUrl()+bean.getName());
+		int start = bean.getName().lastIndexOf(File.separator)+1;
+		int smnStart = bean.getSmallName().lastIndexOf(File.separator)+1;
+		String prx = StringUtils.gerDir(String.valueOf(bean.getArticleId()));
+		String fileName = prx+bean.getArticleId()+File.separator+bean.getName().substring(start);
+		String smallFileName = prx+bean.getArticleId()+File.separator+bean.getSmallName().substring(smnStart);
+		String source = bean.getUrl() + bean.getName();
+		String target = FILE_SERVER+fileName;
+		String smgTarget = FILE_SERVER + smallFileName;
+		bean.setName(fileName);
+		
+		System.out.println("source:"+source);
+		System.out.println("target:"+target);
+		System.out.println("smgTarget:"+smgTarget);
+		if(FileUtils.copyFile(source, target)){
+			System.out.println(">> 大图成功!!!");
+		}
+		
+		if(FileUtils.copyFile(bean.getUrl()+bean.getSmallName(), smgTarget)){
+			System.out.println(">> 小图成功!!!");
+		}else{
+			if(FileUtils.deleteFile(target)){
+				System.out.println(">> 生成小图失败,删除大图");
+			}
+		}
+		
+		
+//		System.out.println("name:"+bean.getName().substring(0, bean.getName().lastIndexOf(File.separator)));
+//		System.out.println("smallname:"+bean.getSmallName());
+	}
+}
