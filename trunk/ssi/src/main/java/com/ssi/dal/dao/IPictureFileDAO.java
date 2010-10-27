a3881bdff787027f837ce003d73274ffa1429884
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2010-10-27 07:15:46 +0000

    增加查询最新N条记录的方法。

--- trunk/ssi/src/main/java/com/ssi/dal/dao/IPictureFileDAO.java
+++ trunk/ssi/src/main/java/com/ssi/dal/dao/IPictureFileDAO.java
@@ -1,5 +1,7 @@
 package com.ssi.dal.dao;
 
+import java.util.List;
+
 import com.ssi.dal.domain.PictureFile;
 
 public interface IPictureFileDAO {
@@ -11,4 +13,11 @@ public interface IPictureFileDAO {
 	 * @return
 	 */
 	PictureFile find(Integer articleId,Integer imageId);
+	
+	/**
+	 * 查找最近多少条记录
+	 * @param number 记录数
+	 * @return
+	 */
+	List<PictureFile> findLastPictureFile(int number);
 }
