b1bab6e5609897fcf6c6339090fed9f4af69e70c
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2010-11-26 05:21:10 +0000

    系统配置数据访问对象接口实现类。

---
+++ trunk/ssi-common-model/src/com/ssi/common/dal/dao/impl/SysConfigDAOImpl.java
@@ -0,0 +1,49 @@
+package com.ssi.common.dal.dao.impl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import com.ssi.common.dal.BaseDAOImpl;
+import com.ssi.common.dal.dao.ISysConfigDAO;
+import com.ssi.common.dal.domain.SysConfig;
+
+public class SysConfigDAOImpl  extends BaseDAOImpl implements ISysConfigDAO {
+
+	public List<SysConfig> find(HashMap map) {
+		List<SysConfig> list = new ArrayList<SysConfig>();
+		list = getQueryDelegate().queryForList("QUERY_SYSCONFIG_BY_MAP", map, getRoute());
+		return list;
+	}
+
+	public SysConfig findById(Integer id) {
+		SysConfig config = null;
+		config = (SysConfig)getQueryDelegate().queryForObject("GET_SYSCONFIG_BY_ID", id, getRoute());
+		return config;
+	}
+
+	public SysConfig findByName(String name) {
+		SysConfig config = null;
+		config = (SysConfig)getQueryDelegate().queryForObject("GET_SYSCONFIG_BY_NAME", name, getRoute());
+		return config;
+	}
+
+	public int getCount(HashMap map) {
+		int result = -1;
+		result = (Integer)getQueryDelegate().queryForObject("GET_SYSCONFIG_COUNT", map, getRoute());
+		return result;
+	}
+
+	public int insert(SysConfig config) {
+		int result = -1;
+		result = (Integer)getEntityDelegate().insert("INSERT_SYSCONFIG", config, getRoute());
+		return result;
+	}
+
+	public int update(SysConfig config) {
+		int result = -1;
+		result = (Integer)getEntityDelegate().update("UPDATE_SYSCONFIG", config, getRoute());
+		return result;
+	}
+
+}
