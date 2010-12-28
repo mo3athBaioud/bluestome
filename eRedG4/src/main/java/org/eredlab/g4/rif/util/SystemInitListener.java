19b2ee5b7b936b1a9011f5b3cb7f122b7aa1cef8
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2010-12-28 09:55:18 +0000

    修改系统监听器中的打印信息由System.out.println修改为log.info

--- eRedG4/src/main/java/org/eredlab/g4/rif/util/SystemInitListener.java
+++ eRedG4/src/main/java/org/eredlab/g4/rif/util/SystemInitListener.java
@@ -6,6 +6,8 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.eredlab.g4.arm.service.MonitorService;
 import org.eredlab.g4.arm.service.ResourceService;
 import org.eredlab.g4.arm.util.ArmConstants;
@@ -25,6 +27,7 @@ import org.springframework.context.ApplicationContext;
  * @since 2010-04-13
  */
 public class SystemInitListener implements ServletContextListener {
+	private static Log log = LogFactory.getLog(SystemInitListener.class);
 	private boolean success = true;
 	private ApplicationContext wac = null;
 
@@ -44,9 +47,9 @@ public class SystemInitListener implements ServletContextListener {
 	    String forceLoad = pHelper.getValue("forceLoad", ArmConstants.FORCELOAD_N);
 		long start = System.currentTimeMillis();
 		if (forceLoad.equalsIgnoreCase(ArmConstants.FORCELOAD_N)) {
-			System.out.println("********************************************");
-			System.out.println("易道系统集成与应用开发平台开始启动...");
-			System.out.println("********************************************");
+			log.info("********************************************");
+			log.info("易道系统集成与应用开发平台开始启动...");
+			log.info("********************************************");
 		}
 		try {
 			wac = SpringBeanLoader.getApplicationContext();
@@ -62,47 +65,47 @@ public class SystemInitListener implements ServletContextListener {
 		Dto dto = new BaseDto();
 		dto.put("enabled", ArmConstants.EDITMODE_Y);
 		if (success) {
-			System.out.println("-------------------------------");
-			System.out.println("系统开始启动代码表装载程序...");
-			System.out.println("开始加载代码表...");
+			log.info("-------------------------------");
+			log.info("系统开始启动代码表装载程序...");
+			log.info("开始加载代码表...");
 			ResourceService resourceService = (ResourceService) SpringBeanLoader.getSpringBean("resourceService");
 			List codeList = null;
 			try {
 				codeList = resourceService.getCodeList(dto).getDefaultAList();
-				System.out.println("代码表加载成功!");
+				log.info("代码表加载成功!");
 			} catch (Exception e) {
 				success = false;
-				System.out.println("代码表加载失败!");
+				log.info("代码表加载失败!");
 				e.printStackTrace();
 			}
 			servletContext.setAttribute("EACODELIST", codeList);
 
 		}
 		if (success) {
-			System.out.println("-------------------------------");
-			System.out.println("系统开始启动全局参数表装载程序...");
-			System.out.println("开始加载全局参数表...");
+			log.info("-------------------------------");
+			log.info("系统开始启动全局参数表装载程序...");
+			log.info("开始加载全局参数表...");
 			List paramList = null;
 			try {
 				ResourceService resourceService = (ResourceService) SpringBeanLoader.getSpringBean("resourceService");
 				paramList = resourceService.getParamList(dto).getDefaultAList();
-				System.out.println("全局参数表加载成功!");
+				log.info("全局参数表加载成功!");
 			} catch (Exception e) {
 				success = false;
-				System.out.println("全局参数表加载失败!");
+				log.info("全局参数表加载失败!");
 				e.printStackTrace();
 			}
 			servletContext.setAttribute("EAPARAMLIST", paramList);
 		}
 		long timeSec = (System.currentTimeMillis() - start) / 1000;
-		System.out.println("********************************************");
+		log.info("********************************************");
 		if (success) {
-			System.out.println("易道系统集成与应用开发平台启动成功[" + G4Utils.getCurrentTime() + "]");
-			System.out.println( "启动总耗时: " + timeSec/60+ "分 " + timeSec%60 + "秒 ");
+			log.info("易道系统集成与应用开发平台启动成功[" + G4Utils.getCurrentTime() + "]");
+			log.info( "启动总耗时: " + timeSec/60+ "分 " + timeSec%60 + "秒 ");
 		} else {
-			System.out.println("易道系统集成与应用开发平台启动失败[" + G4Utils.getCurrentTime() + "]");
-			System.out.println( "启动总耗时: " + timeSec/60+ "分" + timeSec%60 + "秒");
+			log.info("易道系统集成与应用开发平台启动失败[" + G4Utils.getCurrentTime() + "]");
+			log.info( "启动总耗时: " + timeSec/60+ "分" + timeSec%60 + "秒");
 		}
-		System.out.println("********************************************");
+		log.info("********************************************");
 	}
 }
