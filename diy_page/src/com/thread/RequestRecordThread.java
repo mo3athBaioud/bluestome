12b0dad96c90e7225fbb65a223700fcbabeece1b
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-01-05 09:03:49 +0000

    新增线程类，可以后台处理数据入库操作

---
+++ diy_page/src/com/thread/RequestRecordThread.java
@@ -0,0 +1,152 @@
+package com.thread;
+
+import java.util.Vector;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.chinamilitary.bean.Article;
+import com.chinamilitary.bean.ImageBean;
+import com.chinamilitary.bean.ArticleDoc;
+import com.chinamilitary.bean.PicfileBean;
+import com.chinamilitary.factory.DAOFactory;
+
+public class RequestRecordThread extends Thread {
+	
+	private static Log logger = LogFactory.getLog(RequestRecordThread.class);
+	
+	private int count = 0;
+	
+	private int count2 = 0;
+	
+	private boolean isRunning = true;
+	
+	private long sleeptime = 0;
+	
+	public RequestRecordThread(){
+		System.out.println("获取队列入库记录线程启动!!!");
+		sleeptime = Integer.valueOf(10000);
+	}
+	
+	
+	public synchronized void run() {
+		try {
+			while (isRunning) {
+				count = ResourceQueneInsert.getInstance().getQuene().size();
+				count2 = ResourceQueneUpdate.getInstance().getQuene().size();
+				System.out.println(" >> 队列数:"+count);
+				if(count > 0 || count2 > 0){
+					executeInsert();
+					executeUpdate();
+				}else if( count % 400 == 0 || count2 % 400 == 0){
+					pause();
+				}
+			}
+		} catch (Exception e) {
+			System.err.println("RecordThread.run.exception:"+e.getMessage());
+			try{
+				ResourceQueneInsert.getInstance().getQuene().clear();
+				ResourceQueneUpdate.getInstance().getQuene().clear();
+				pause();
+			}catch(Exception ex){
+				System.err.println(" >> pause exception:"+ex);
+			}
+		}
+	}
+	
+	public void pause() throws Exception{
+		isRunning = false;
+		Thread.sleep(sleeptime);
+		isRunning = true;
+	}
+
+	/**
+	 * 获取队列中的入库记录
+	 * @throws Exception
+	 */
+	public synchronized void executeInsert() throws Exception {
+		Object obj = ResourceQueneInsert.getInstance().getElement();
+		if(null != obj){
+			if(obj instanceof Article){
+				System.out.println(" >> Article Insert");
+			}else if(obj instanceof ImageBean){
+				System.out.println(" >> ImageBean Insert");
+			}else if(obj instanceof ArticleDoc){
+				ArticleDoc doc = (ArticleDoc)obj;
+				try{
+					int result = DAOFactory.getInstance().getArticleDocDao().insert(doc);
+					System.out.println(" >> ArticleDoc Insert ["+result+"] ");
+				}catch(Exception e){
+					System.err.println(" >> executeInsert.ArticleDoc.Exception:"+e);
+				}
+			}else if(obj instanceof PicfileBean){
+				System.out.println(" >> PicfileBean Insert");
+			}else if(obj instanceof String){
+				String str = (String)obj;
+				System.out.println(" >> Unknow Object:"+str);
+			}
+		}
+	}
+
+	/**
+	 * 获取队列中的入库记录
+	 * @throws Exception
+	 */
+	public synchronized void executeUpdate() throws Exception {
+		Object obj = ResourceQueneUpdate.getInstance().getElement();
+		if(null != obj){
+			if(obj instanceof Article){
+				Article art = (Article)obj;
+				System.out.println(" >> Article["+art.getTitle()+"] Update!");
+			}else if(obj instanceof ImageBean){
+				ImageBean img = (ImageBean)obj;
+				System.out.println(" >> ImageBean["+img.getHttpUrl()+"] Update!");
+			}else if(obj instanceof ArticleDoc){
+				ArticleDoc doc = (ArticleDoc)obj;
+				try{
+					if(DAOFactory.getInstance().getArticleDocDao().update(doc)){
+						System.out.println(" >> ArticleDoc["+doc.getTitle()+"] Update Success!");
+					}
+				}catch(Exception e){
+					System.err.println(" >> executeUpdate.ArticleDoc.Exception:"+e);
+				}
+			}else if(obj instanceof PicfileBean){
+				PicfileBean pic = (PicfileBean)obj;
+				System.out.println(" >> PicfileBean["+pic.getArticleId()+"] Update!");
+			}else{
+				System.out.println(" >> Unknow Object");
+			}
+		}
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+
+	public boolean isRunning() {
+		return isRunning;
+	}
+
+
+	public void setRunning(boolean isRunning) {
+		this.isRunning = isRunning;
+	}
+
+
+	public long getSleeptime() {
+		return sleeptime;
+	}
+
+
+	public void setSleeptime(long sleeptime) {
+		this.sleeptime = sleeptime;
+	}
+
+	
+}
