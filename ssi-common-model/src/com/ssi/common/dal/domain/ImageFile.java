6403a9410263c736313a85121b7c22c59d66bfd9
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-03-12 12:09:48 +0000

    2011-03-12 新增图片文件对象

---
+++ ssi-common-model/src/com/ssi/common/dal/domain/ImageFile.java
@@ -0,0 +1,142 @@
+package com.ssi.common.dal.domain;
+
+import java.util.Date;
+
+/**
+ * 图片文件对象 tbl_pic_file
+ * 
+ * @author bluestome
+ * 
+ */
+public class ImageFile extends AbstractEntity {
+
+	private static final long serialVersionUID = 3651555464532842680L;
+
+	private Integer id;
+
+	private Integer articleId;
+
+	private Integer imageId;
+
+	private String url;
+
+	private String name;
+
+	private String smallName;
+
+	private String title;
+
+	private String desc;
+
+	private Date createTime = new Date();
+
+	// 文章对象
+	private Article article;
+
+	// 图片对象
+	private Image image;
+
+	public ImageFile() {
+
+	}
+
+	public ImageFile(Integer articleId, Integer imageId, String url,
+			String name, String desc, String title) {
+		this.articleId = articleId;
+		this.imageId = imageId;
+		this.url = url;
+		this.name = name;
+		this.title = title;
+		this.desc = desc;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Integer getArticleId() {
+		return articleId;
+	}
+
+	public void setArticleId(Integer articleId) {
+		this.articleId = articleId;
+	}
+
+	public Integer getImageId() {
+		return imageId;
+	}
+
+	public void setImageId(Integer imageId) {
+		this.imageId = imageId;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public String getSmallName() {
+		return smallName;
+	}
+
+	public void setSmallName(String smallName) {
+		this.smallName = smallName;
+	}
+
+	public Article getArticle() {
+		return article;
+	}
+
+	public void setArticle(Article article) {
+		this.article = article;
+	}
+
+	public Image getImage() {
+		return image;
+	}
+
+	public void setImage(Image image) {
+		this.image = image;
+	}
+
+	
+}
