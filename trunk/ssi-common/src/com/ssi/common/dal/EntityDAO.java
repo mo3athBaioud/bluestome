95e9560b729fa35566865a8fde88800c58a8f9b8
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-03-14 02:27:23 +0000

    新增通用DAO类。

---
+++ trunk/ssi-common/src/com/ssi/common/dal/EntityDAO.java
@@ -0,0 +1,78 @@
+package com.ssi.common.dal;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 实体DAO
+ * 
+ * @author bluestome
+ * 
+ * @param <T>
+ */
+public interface EntityDAO<T> {
+	/**
+	 * 根据主键查找对象
+	 * 
+	 * @param id
+	 *            主键值
+	 * @return 对象实体
+	 */
+	T findByPrimarykey(Serializable id);
+
+	/**
+	 * 新增对象到数据库
+	 * 
+	 * @param o
+	 *            对象实体
+	 */
+	Integer insert(T o);
+
+	/**
+	 * 更新对象实体到数据库
+	 * 
+	 * @param o
+	 *            对象实体
+	 */
+	Integer update(T o);
+
+	/**
+	 * 根据主键删除对象
+	 * 
+	 * @param id
+	 *            主键值
+	 */
+	void deleteByPrimarykey(Serializable id);
+
+	/**
+	 * 更新对象信息
+	 * 
+	 * @param statementId
+	 *            sql语句名称后缀
+	 * @param parameters
+	 *            sql参数
+	 */
+	Integer update(String statementId, Object parameters);
+
+	/**
+	 * sql查询单个对象
+	 * 
+	 * @param statementId
+	 *            sql语句名称后缀
+	 * @param parameters
+	 *            sql参数
+	 * @return 查询结果
+	 */
+	T queryForObject(String statementId, Object parameters);
+
+	/**
+	 * sql查询列表
+	 * 
+	 * @param statementId
+	 *            sql语句名称后缀
+	 * @param parameters
+	 *            sql参数
+	 * @return 查询结果
+	 */
+	List<T> queryForList(String statementId, Object parameters);
+}
---
+++ trunk/ssi-common/src/com/ssi/common/dal/Page.java
@@ -0,0 +1,15 @@
+package com.ssi.common.dal;
+
+import java.util.List;
+
+public class Page {
+
+	public Page(int startIndex, Integer totalCount, int limit, List list) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Page() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
