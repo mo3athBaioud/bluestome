5c51f226975ca469bba4785a0ee53d673eeab670
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-03-14 10:31:47 +0000

    2011-03-14 修改主页面中展现的数据为站点管理。

--- ssi-cms/web/scripts/main.js
+++ ssi-cms/web/scripts/main.js
@@ -311,7 +311,7 @@ Ext.onReady(function() {
 				margins : '2 5 5 0',
 				activeItem : 0,
 				border : false,
-				html:'<iframe name="mainFrame" width="98%" height="800" id="mainFrame" frameborder=0 src="info.html"></iframe>'					
+				html:'<iframe name="mainFrame" width="98%" height="800" id="mainFrame" frameborder=0 src="pages/websites/website.jsp"></iframe>'					
 			}]
 		});
 	}, 250);
