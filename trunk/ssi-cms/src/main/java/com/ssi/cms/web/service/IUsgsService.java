e74faa22b893b093b12abdcb46581fbc2c9f909b
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-03-15 15:13:53 +0000

    2011-03-15 新增分页方法

--- trunk/ssi-cms/src/main/java/com/ssi/cms/web/service/IUsgsService.java
+++ trunk/ssi-cms/src/main/java/com/ssi/cms/web/service/IUsgsService.java
@@ -21,4 +21,23 @@ public interface IUsgsService {
 	 * @return
 	 */
 	int getCount(HashMap map);
+	
+	/**
+	 * 根据列名和值来查询数据
+	 * @param colName
+	 * @param value
+	 * @return
+	 */
+	int getCount(String colName,String value);
+
+	
+	/**
+	 * 分页方法
+	 * @param pageNo 页码
+	 * @param pageSize 每页显示的记录数
+	 * @param asc 是否顺序排列
+	 * @return List<T>
+	 */
+	List<EarthQuakeInfo> getPageList(String colName,String value,Integer startIndex,Integer pageSize,boolean asc) throws Exception;
+	
 }
