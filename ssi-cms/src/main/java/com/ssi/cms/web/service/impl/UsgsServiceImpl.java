40a86ca3cc63ed872b7ae789bec344f2e96973b8
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-03-15 15:14:19 +0000

    2011-03-15 新增分页方法的实现

--- ssi-cms/src/main/java/com/ssi/cms/web/service/impl/UsgsServiceImpl.java
+++ ssi-cms/src/main/java/com/ssi/cms/web/service/impl/UsgsServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 
 import com.ssi.cms.web.service.IUsgsService;
+import com.ssi.common.dal.domain.Website;
 import com.ssi.dal.usgs.dao.IEarthQuakeDetailDAO;
 import com.ssi.dal.usgs.dao.IEarthQuakeInfoDAO;
 import com.ssi.dal.usgs.domain.EarthQuakeInfo;
@@ -46,6 +47,39 @@ public class UsgsServiceImpl implements IUsgsService {
 
 	public void setEarthQuakeInfoDAO(IEarthQuakeInfoDAO earthQuakeInfoDAO) {
 		this.earthQuakeInfoDAO = earthQuakeInfoDAO;
+	}
+
+	public List<EarthQuakeInfo> getPageList(String colName, String value, Integer startIndex, Integer pageSize, boolean asc) throws Exception {
+		HashMap map = new HashMap();
+		if(null == colName || colName.equalsIgnoreCase("")){
+			map.put("magnitude", value);
+		}
+		if(null == startIndex){
+			startIndex = 0;
+		}
+		if(null == pageSize){
+			pageSize = 20;
+		}
+		map.put(colName, value);
+		map.put("limit", pageSize);
+		map.put("offset", startIndex);
+		if(asc){
+			map.put("asc", "asc");
+		}else{
+			map.put("asc", "desc");
+		}
+		List<EarthQuakeInfo>  list = earthQuakeInfoDAO.find(map);
+		return list;
+	}
+
+	public int getCount(String colName, String value) {
+		HashMap map = new HashMap();
+		if(null != colName && !"".equals(colName)){
+			map.put(colName, value);
+		}else{
+			map.put("magnitude", value);
+		}
+		return earthQuakeInfoDAO.getCount(map);
 	}
 	
 }
