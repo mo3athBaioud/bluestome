ec9d53a74bec5ba3fbca8b56b857b9b83b597d8a
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-04-06 15:01:47 +0000

    2011-04-06 新增页面解析接口

---
+++ ssi-common/src/com/ssi/common/htmlparser/parser/AbstractHtmlParser.java
@@ -0,0 +1,27 @@
+package com.ssi.common.htmlparser.parser;
+
+import com.ssi.common.htmlparser.bean.LinkPage;
+
+/**
+ * 解析抽象类
+ * @author bluestome
+ *
+ */
+public interface AbstractHtmlParser {
+	
+	/**
+	 * 获取分类
+	 * @param url
+	 * @return
+	 * @throws Exception
+	 */
+	LinkPage catalog(String url) throws Exception;
+
+	/**
+	 * 获取分页
+	 * @param url
+	 * @return
+	 * @throws Exception
+	 */
+	LinkPage page(String url) throws Exception;
+}
