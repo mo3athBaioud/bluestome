eacc88d6bc8f801eb1ac81e4f7c40666bd7de56f
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-04-10 13:21:51 +0000

    2011-04-10 添加新的手机终端表对象和DAO

---
+++ trunk/ssi-common-model/src/com/ssi/mobile/dal/dao/IMobileBrandDAO.java
@@ -0,0 +1,8 @@
+package com.ssi.mobile.dal.dao;
+
+import com.ssi.common.dal.EntityDAO;
+import com.ssi.mobile.dal.domain.MobileBrand;
+
+public interface IMobileBrandDAO extends EntityDAO<MobileBrand> {
+
+}
---
+++ trunk/ssi-common-model/src/com/ssi/mobile/dal/dao/impl/MobileBrandDAOImpl.java
@@ -0,0 +1,10 @@
+package com.ssi.mobile.dal.dao.impl;
+
+import com.ssi.common.dal.IbatisEntityDao;
+import com.ssi.mobile.dal.dao.IMobileBrandDAO;
+import com.ssi.mobile.dal.domain.MobileBrand;
+
+public class MobileBrandDAOImpl extends IbatisEntityDao<MobileBrand> implements
+		IMobileBrandDAO {
+
+}
