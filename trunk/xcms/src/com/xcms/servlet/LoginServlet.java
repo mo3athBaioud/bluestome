eba349754b43f4940bbe29837e4b208fb467080a
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-04-20 10:50:57 +0000

    2011-04-20 修改信息输出为日志打印输出

--- trunk/xcms/src/com/xcms/servlet/LoginServlet.java
+++ trunk/xcms/src/com/xcms/servlet/LoginServlet.java
@@ -1,15 +1,27 @@
 package com.xcms.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public class LoginServlet extends HttpServlet {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected final Log logger = LogFactory.getLog(this.getClass());
+	
 	static String[][] USERS = {{"liyang","liyang123"},{"zhangxiao","zhangxiao123"}};
 	/**
 	 * Constructor of the object.
@@ -59,6 +71,7 @@ public class LoginServlet extends HttpServlet {
 		if(name.equals(USERS[0][0]) ){
 			if(password.equals(USERS[0][1])){
 				request.getSession().setAttribute("LOGIN_SESSION_NAME", USERS[0][0]);
+				logger.info(" User["+USERS[0][0]+"] login at "+getStringDate());
 				response.getWriter().print("{success:true,msg:'登录成功',url:'"+request.getContextPath()+"index.html'}");
 				return;
 			}else{
@@ -70,6 +83,7 @@ public class LoginServlet extends HttpServlet {
 		if(name.equals(USERS[1][0])){
 			if(password.equals(USERS[1][1]) ){
 				request.getSession().setAttribute("LOGIN_SESSION_NAME", USERS[1][0]);
+				logger.info(" User["+USERS[1][0]+"] login at "+getStringDate());
 				response.getWriter().print("{success:true,msg:'登录成功',url:'"+request.getContextPath()+"/index.html'}");
 				return;
 			}else{
@@ -89,6 +103,18 @@ public class LoginServlet extends HttpServlet {
 	 */
 	public void init() throws ServletException {
 		// Put your code here
+	}
+	
+	/**
+	 * 获取现在时间
+	 * 
+	 * @return 返回字符串格式 yyyy-MM-dd HH:mm:ss
+	 */
+	public static String getStringDate() {
+		Date currentTime = new Date();
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String dateString = formatter.format(currentTime);
+		return dateString;
 	}
 
 }
