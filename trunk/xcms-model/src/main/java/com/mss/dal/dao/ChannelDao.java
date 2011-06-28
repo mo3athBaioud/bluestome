f3c33deb11ef20bed709330ac41d2e65e47d1438
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-06-28 13:56:00 +0000

    2011-06-28 提交新的对象

---
+++ trunk/xcms-model/src/main/java/com/mss/dal/dao/ChannelDao.java
@@ -0,0 +1,13 @@
+package com.mss.dal.dao;
+
+import org.nutz.ioc.loader.annotation.IocBean;
+
+/**
+ * 渠道DAO
+ * @author bluestome
+ *
+ */
+@IocBean
+public class ChannelDao extends BasicDao {
+
+}
---
+++ trunk/xcms-model/src/main/java/com/mss/dal/dao/StaffDao.java
@@ -0,0 +1,13 @@
+package com.mss.dal.dao;
+
+import org.nutz.ioc.loader.annotation.IocBean;
+
+/**
+ * 员工DAO
+ * @author bluestome
+ *
+ */
+@IocBean
+public class StaffDao extends BasicDao {
+
+}
