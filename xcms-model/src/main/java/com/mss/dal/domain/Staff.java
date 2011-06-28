c99f9c68db8659b20a670565983b46c4afa0fcac
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-06-28 13:56:37 +0000

    2011-06-28 提交新的对象2

---
+++ xcms-model/src/main/java/com/mss/dal/domain/Staff.java
@@ -0,0 +1,122 @@
+package com.mss.dal.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.nutz.dao.entity.annotation.Column;
+import org.nutz.dao.entity.annotation.Id;
+import org.nutz.dao.entity.annotation.Name;
+import org.nutz.dao.entity.annotation.One;
+import org.nutz.dao.entity.annotation.Table;
+
+@Table("tbl_staff")
+public class Staff implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column("d_id")
+	private Integer id;
+
+	@Column("d_username")
+	private String username;
+
+	@Column("d_password")
+	private String password;
+	
+	@Column("d_mobile")
+	private String mobile;
+	
+	@Column("d_officephone")
+	private String officephone;
+	
+//	@Name
+	@Column("d_channel_code")
+	private String channelcode;
+	
+	@Column("d_status")
+	private Integer status = 1;
+	
+	@Column("d_createtime")
+	private Date createtime = new Date();
+	
+//	@One(target = Channel.class,field = "channelcode")
+	private Channel channel;
+
+	public String getChannelcode() {
+		return channelcode;
+	}
+
+	public void setChannelcode(String channelcode) {
+		this.channelcode = channelcode;
+	}
+
+	public Date getCreatetime() {
+		return createtime;
+	}
+
+	public void setCreatetime(Date createtime) {
+		this.createtime = createtime;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getOfficephone() {
+		return officephone;
+	}
+
+	public void setOfficephone(String officephone) {
+		this.officephone = officephone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Channel getChannel() {
+		return channel;
+	}
+
+	public void setChannel(Channel channel) {
+		this.channel = channel;
+	}
+
+	
+}
