84c1f9937aa02abd0a7bfa3a46fe18c45aeeca3f
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-06-28 13:56:00 +0000

    2011-06-28 提交新的对象

---
+++ xcms-model/src/main/java/com/mss/dal/dao/BDistrictDao.java
@@ -0,0 +1,13 @@
+package com.mss.dal.dao;
+
+import org.nutz.ioc.loader.annotation.IocBean;
+
+/**
+ * 业务区DAO
+ * @author bluestome
+ *
+ */
+@IocBean
+public class BDistrictDao extends BasicDao {
+
+}
