ffe377adf9f82fb35c6e5986a8ae38ce8c2cff81
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-07-03 17:13:20 +0000

    2011-07-04 提交新model

---
+++ trunk/xcms-model/src/main/java/com/mss/dal/dao/BussinessDao.java
@@ -0,0 +1,13 @@
+package com.mss.dal.dao;
+
+import org.nutz.ioc.loader.annotation.IocBean;
+
+/**
+ * 业务数据DAO
+ * @author bluestome
+ *
+ */
+@IocBean
+public class BussinessDao extends BasicDao {
+
+}
---
+++ trunk/xcms-model/src/main/java/com/mss/dal/domain/Bussiness.java
@@ -0,0 +1,120 @@
+package com.mss.dal.domain;
+
+import java.io.Serializable;
+
+import org.nutz.dao.entity.annotation.Column;
+import org.nutz.dao.entity.annotation.Id;
+import org.nutz.dao.entity.annotation.Readonly;
+import org.nutz.dao.entity.annotation.Table;
+
+@Table("tbl_bussiness")
+public class Bussiness implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column("d_id")
+	private Integer id;
+	
+	@Column("d_phonenum")
+	private String phonenum;
+	
+	@Column("d_btype")
+	private Integer btype;
+	
+	@Column("d_hsman")
+	private String hsman;
+	
+	@Column("d_hstype")
+	private String hstype;
+	
+	@Column("d_imei")
+	private String imei;
+	
+	@Column("d_support")
+	private Integer support;
+	
+	@Column("d_support_type")
+	private Integer suuporttype;
+	
+	//需要从日志表中获取统计的数据
+	@Readonly
+	private Integer qcount = 0;
+
+	public Integer getBtype() {
+		return btype;
+	}
+
+	public void setBtype(Integer btype) {
+		this.btype = btype;
+	}
+
+	public String getHsman() {
+		return hsman;
+	}
+
+	public void setHsman(String hsman) {
+		this.hsman = hsman;
+	}
+
+	public String getHstype() {
+		return hstype;
+	}
+
+	public void setHstype(String hstype) {
+		this.hstype = hstype;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getImei() {
+		return imei;
+	}
+
+	public void setImei(String imei) {
+		this.imei = imei;
+	}
+
+	public String getPhonenum() {
+		return phonenum;
+	}
+
+	public void setPhonenum(String phonenum) {
+		this.phonenum = phonenum;
+	}
+
+	public Integer getQcount() {
+		return qcount;
+	}
+
+	public void setQcount(Integer qcount) {
+		this.qcount = qcount;
+	}
+
+	public Integer getSupport() {
+		return support;
+	}
+
+	public void setSupport(Integer support) {
+		this.support = support;
+	}
+
+	public Integer getSuuporttype() {
+		return suuporttype;
+	}
+
+	public void setSuuporttype(Integer suuporttype) {
+		this.suuporttype = suuporttype;
+	}
+	
+	
+}
