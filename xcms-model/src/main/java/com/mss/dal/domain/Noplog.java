4f2bb228bbee253c1203b75059c743aadb8fe164
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-07-04 18:00:24 +0000

    2011-07-05 新增model类。

---
+++ xcms-model/src/main/java/com/mss/dal/domain/Noplog.java
@@ -0,0 +1,118 @@
+package com.mss.dal.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.nutz.dao.entity.annotation.Column;
+import org.nutz.dao.entity.annotation.Id;
+import org.nutz.dao.entity.annotation.Table;
+
+@Table("tbl_noplog")
+public class Noplog implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column("d_id")
+	private Integer id;
+	
+	@Column("d_uid")
+	private Integer uid;
+	
+	@Column("d_phonenum")
+	private String phonenum;
+	
+	@Column("d_phonenum_bdistrict")
+	private String phonenumBDistrict;
+	
+	@Column("d_btype")
+	private Integer btype;
+	
+	@Column("d_loginname")
+	private String loginname;
+	
+	@Column("d_loginname_bdistrict")
+	private String loginnameBDistrict;
+	
+	@Column("d_createtime")
+	private Date createtime;
+	
+	@Column("d_result")
+	private Integer result;
+
+	public Integer getBtype() {
+		return btype;
+	}
+
+	public void setBtype(Integer btype) {
+		this.btype = btype;
+	}
+
+	public Date getCreatetime() {
+		return createtime;
+	}
+
+	public void setCreatetime(Date createtime) {
+		this.createtime = createtime;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLoginname() {
+		return loginname;
+	}
+
+	public void setLoginname(String loginname) {
+		this.loginname = loginname;
+	}
+
+	public String getLoginnameBDistrict() {
+		return loginnameBDistrict;
+	}
+
+	public void setLoginnameBDistrict(String loginnameBDistrict) {
+		this.loginnameBDistrict = loginnameBDistrict;
+	}
+
+	public String getPhonenum() {
+		return phonenum;
+	}
+
+	public void setPhonenum(String phonenum) {
+		this.phonenum = phonenum;
+	}
+
+	public String getPhonenumBDistrict() {
+		return phonenumBDistrict;
+	}
+
+	public void setPhonenumBDistrict(String phonenumBDistrict) {
+		this.phonenumBDistrict = phonenumBDistrict;
+	}
+
+	public Integer getResult() {
+		return result;
+	}
+
+	public void setResult(Integer result) {
+		this.result = result;
+	}
+
+	public Integer getUid() {
+		return uid;
+	}
+
+	public void setUid(Integer uid) {
+		this.uid = uid;
+	}
+	
+}
