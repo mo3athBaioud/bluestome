5dd2ad1f6bda2ebe495789d6908bf9b9908b20ba
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-07-04 17:58:43 +0000

    2011-07-05 提交文件解析类。

--- xcms-tools/src/main/java/com/xc/tools/CvsFileParser.java
+++ xcms-tools/src/main/java/com/xc/tools/CvsFileParser.java
@@ -62,4 +62,45 @@ public class CvsFileParser {
 		}
 		return list;
 	}
+
+	/**
+	 * 返回CSV文件中的数据列表，并以数据的形式返回
+	 * @param fileName
+	 * s[0] = 统计日期 实际数据中不需要
+	 * s[1] = 用户标识
+	 * s[2] = 手机号码
+	 * s[3] = imei号码
+	 * s[4] = 业务区编码
+	 * s[5] = 通话次数
+	 * s[6] = 基本计费跳
+	 * s[7] = 通话时长
+	 * s[8] = tac码
+	 * @return
+	 */
+	public static List<String[]> getCSVAbsPath(String fileName){
+		List<String[]> list = new ArrayList<String[]>();
+		System.gc();
+		try{
+			Thread.sleep(1000);
+			int u = 100002;
+			int i = 0;
+			String tmp = IOUtil.readFile(fileName, "GBK", ".csv");
+			if(null != tmp && !"".equals(tmp)){
+				String[] rows = tmp.split("\r\n");
+				int count = rows.length;
+				for(String row:rows){
+					if(i < u ){
+						String[] s = row.replace("\"", "").trim().split(",");
+						list.add(s);
+						i++;
+					}else{
+						break;
+					}
+				}
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return list;
+	}
 }
