3434de9f85d9df8e397613792576cc0849e87c42
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-07-04 18:00:55 +0000

    2011-07-05 新增DAO类。

---
+++ trunk/xcms-model/src/main/java/com/mss/dal/dao/NOplogDao.java
@@ -0,0 +1,13 @@
+package com.mss.dal.dao;
+
+import org.nutz.ioc.loader.annotation.IocBean;
+
+/**
+ * 操作日志DAO
+ * @author bluestome
+ *
+ */
+@IocBean
+public class NOplogDao extends BasicDao {
+
+}
