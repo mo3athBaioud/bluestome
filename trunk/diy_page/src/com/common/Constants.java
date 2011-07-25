0e36cb178e6d6dc6cefa958cb57c8649f5905103
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-07-25 09:37:01 +0000

    2011-07-25 新增DOC文件保存路径。

--- trunk/diy_page/src/com/common/Constants.java
+++ trunk/diy_page/src/com/common/Constants.java
@@ -4,4 +4,7 @@ public class Constants {
 
 	//文件保存路径
 	public static final String FILE_SERVER = "D:/fileserver/image/";
+
+	//文件保存路径
+	public static final String DOC_FILE_SERVER = "D:/fileserver/doc/";
 }
