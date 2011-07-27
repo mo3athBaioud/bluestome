00489a74b90c3cbaabd921bf503472a5ed3dbcfd
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-07-27 01:34:40 +0000

    2011-07-27 修改查询条件。

--- trunk/ssi/src/main/java/com/ssi/web/action/IndexAction.java
+++ trunk/ssi/src/main/java/com/ssi/web/action/IndexAction.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.validator.GenericValidator;
+
 import com.ssi.common.dal.domain.Website;
 import com.ssi.web.services.IWebsiteService;
 
@@ -21,13 +23,21 @@ public class IndexAction extends BaseAction {
 	 * @return
 	 */
 	public String index(){
+		int pageSize = 10;
+		String pageIndexName = new org.displaytag.util.ParamEncoder("row").encodeParameterName(org.displaytag.tags.TableTagParameters.PARAMETER_PAGE);
+		int tmpPageIndex = GenericValidator.isBlankOrNull(request.getParameter(pageIndexName)) ? 0:Integer.parseInt(request.getParameter(pageIndexName))-1;
+		System.out.println("pageName:"+pageIndexName+"\tpageValue:"+tmpPageIndex);
 		HashMap map = new HashMap();
 		if(null == id || id < 1){
 			map.put("parentId",0);
 		}else{
 			map.put("parentId",id);
 		}
+		int count =  websiteService.getCount(map);
+		map.put("limit", pageSize);
+		map.put("offset", pageSize*tmpPageIndex);
 		List<Website> list = websiteService.find(map); 
+		request.setAttribute("resultSize", count);
 		request.setAttribute("websiteList", list);
 		return "main";
 	}
