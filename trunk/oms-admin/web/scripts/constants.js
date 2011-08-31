762876b5b5bd4e49e3366d34b3a6ee06a3081bc3
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-08-31 08:39:43 +0000

    2011-08-31 新增系统使用到的常量数据。

--- trunk/oms-admin/web/scripts/constants.js
+++ trunk/oms-admin/web/scripts/constants.js
@@ -3,4 +3,16 @@
  * 作者:bluestome.zhang
  * 
  */
+
+//公司创立年
+var syear = '2005';
+//最新年份
+var eyear = '2011';
+//公司名称
+var companyname = '杭州斯凯网络科技有限公司';
+//项目名称
 var projectname = '斯凯2.0群客后台管理系统';
+//版权说明
+var copyright = 'Copyright&copy '+eyear+' '+companyname;
+//欢迎词
+var welcomeword = '欢迎您,';
