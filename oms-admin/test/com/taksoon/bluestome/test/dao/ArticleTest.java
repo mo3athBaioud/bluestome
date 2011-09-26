83235a2e68df9bc06925e499c71810699a92024b
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-09-26 17:11:12 +0000

    2011-09-27 提交最新代码

---
+++ oms-admin/test/com/taksoon/bluestome/test/dao/ArticleTest.java
@@ -0,0 +1,85 @@
+package com.taksoon.bluestome.test.dao;
+
+import java.util.List;
+
+import org.apache.xbean.spring.context.ClassPathXmlApplicationContext;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+
+import com.takesoon.oms.ssi.entity.Article;
+import com.takesoon.oms.ssi.service.ArticleManager;
+import com.takesoon.oms.ssi.utils.ExtUtil;
+
+public class ArticleTest {
+
+	private ArticleManager articleManager;
+	
+	@Before
+	public void init(){
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		articleManager = (ArticleManager)context.getBean("articleManager");
+	}
+	
+	@After
+	public void destory(){
+		if(null != articleManager)
+			articleManager = null;
+	}
+	
+	public void test(){
+		Article entity = new Article();
+		entity.setId(135);
+		if(null != articleManager){
+			System.out.println("articleManager is not null");
+			int c = articleManager.getCount(entity);
+			System.out.println(" > c:" + c);
+		}
+		
+	}
+	
+	public void get(){
+		Integer id = 135;
+		Article entity = articleManager.get(id);
+		if(null != entity){
+			System.out.println(" > entity.id:"+entity.getId());
+			System.out.println(" > entity.webId:"+entity.getWebId());
+			System.out.println(" > entity.title:"+entity.getTitle());
+		}else{
+			System.err.println(" > error");
+		}
+	}
+	
+	public void getTotal(){
+		Article entity = new Article();
+		int t = articleManager.getTotal(entity);
+		System.out.println(" > t:" + t);
+	}
+	
+	@Test
+	public void buildCountSQL(){
+		Article entity = new Article();
+//		entity.setId(13);
+//		entity.setArticleUrl("military");
+//		entity.setActicleRealUrl("http://");
+//		entity.setActicleXmlUrl("xml");
+//		entity.setIntro(null);
+//		entity.setText("FD");
+		entity.setWebId(36);
+		String sql = articleManager.buildCountSQL(entity);
+		System.out.println(" > sql:" + sql);
+		int c = articleManager.getTotalBySql(entity);
+		System.out.println(" > c:"+c);
+		List<Article> list = articleManager.getListBySql(entity, 0, 200);
+//		for(Article art:list){
+//			System.out.println(" > art.toJson():"+art.toJson());
+//		}
+		String json = ExtUtil.getJsonFromList(c, list);
+		System.out.println(" > json:"+json);
+		
+		String xml = ExtUtil.getXmlFromList(c, list);
+		System.out.println(" > xml:"+xml);
+		
+	}
+}
