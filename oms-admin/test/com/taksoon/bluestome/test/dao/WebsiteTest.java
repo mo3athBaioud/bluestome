400c1f6a5608d0557da35851e27f7c1daf7ea53d
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-10-10 06:19:12 +0000

    2011-10-10 新增网站对象测试类。

---
+++ oms-admin/test/com/taksoon/bluestome/test/dao/WebsiteTest.java
@@ -0,0 +1,62 @@
+package com.taksoon.bluestome.test.dao;
+
+import java.util.List;
+
+import org.apache.xbean.spring.context.ClassPathXmlApplicationContext;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+
+import com.takesoon.oms.ssi.entity.Website;
+import com.takesoon.oms.ssi.service.ArticleManager;
+import com.takesoon.oms.ssi.service.WebsiteManager;
+
+public class WebsiteTest {
+
+	private WebsiteManager websiteManager;
+	
+	@Before
+	public void init(){
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		websiteManager = (WebsiteManager)context.getBean("websiteManager");
+	}
+	
+	@After
+	public void destory(){
+		if(null != websiteManager)
+			websiteManager = null;
+	}
+	
+	public void test(){
+		if(null != websiteManager){
+			System.out.println(" > websiteManager is not null");
+		}
+	}
+	
+	@Test
+	public void getList(){
+		int parentId = 0;
+		getList(parentId);
+	}
+	
+	public void getList(int parentId){
+		Website entity = new Website();
+		entity.setParentId(parentId);
+		List<Website> list = websiteManager.getList(entity, 0, 200);
+		for(Website tmp:list){
+			Website ttmp = new Website();
+			ttmp.setParentId(tmp.getId());
+			int count = websiteManager.getCount(ttmp);
+			System.out.println("[" + tmp.getId()+ "]  > " +  count );
+//			/**
+			if(count > 0){
+				getList(tmp.getId());
+			}else{
+				System.out.println(" >> " + tmp.toJson());
+			}
+//			**/
+			ttmp = null;
+		}
+	}
+}
