56c952a68ccb707ef931e85fb70462d8238f7b55
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-10-13 08:20:44 +0000

    2011-10-13 1.新增间接获取图片显示给前台的方法 2.新增间接获取图片缩略图显示给前台的方法。

--- oms-admin/src/com/takesoon/oms/ssi/action/ImageAction.java
+++ oms-admin/src/com/takesoon/oms/ssi/action/ImageAction.java
@@ -5,12 +5,15 @@ import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.List;
 
+import javax.servlet.ServletOutputStream;
+
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
 import org.apache.struts2.convention.annotation.Result;
 import org.apache.struts2.convention.annotation.Results;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.ssi.common.utils.HttpClientUtils;
 import com.takesoon.oms.ssi.common.Constants;
 import com.takesoon.oms.ssi.common.JMagickScale;
 import com.takesoon.oms.ssi.entity.Article;
@@ -275,6 +278,93 @@ public class ImageAction extends CRUDActionSupport {
 				out.close();
 			}
 		}
+	}
+	
+	/**
+	 * 间接将图片发送给前台
+	 *
+	 */
+	public void image() throws IOException{
+		response.setCharacterEncoding(Constants.CHARSET);
+		ServletOutputStream out = null;
+		//TODO可以使用缓存方式存储已经下载的图片
+		try{
+			 out = response.getOutputStream();
+			 Article art = articleManager.get(entity.getArticleId());
+			 if(null != art)
+			 {
+				 entity = imageManager.get(entity.getId());
+				 //TODO 设置图片类型
+			 	String url = entity.getHttpUrl();
+				if (url.toLowerCase().endsWith(".gif")) {
+					response.setContentType("image/gif;");
+				} else if (url.toLowerCase().endsWith(".png")) {
+					response.setContentType("image/png;");
+				} else if (url.toLowerCase().endsWith(".jpg")){
+					response.setContentType("image/jpg;");
+				}else{
+					response.setContentType("image/jpg;");
+				}
+				byte[] body = HttpClientUtils.getResponseBodyAsByte(art.getArticleUrl(), null, url);
+				if(null != body && body.length > 0)
+				{
+					response.setContentLength(body==null?0:body.length);
+					out.write(body);
+				}
+			 }
+			 //TODO 图片类型
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			if(null != out){
+				out.flush();
+				out.close();
+			}
+		}
+	}
+	
+	/**
+	 * 间接将图片发送给前台
+	 *
+	 */
+	public void icon() throws IOException{
+		ServletOutputStream out = null;
+		//TODO可以使用缓存方式存储已经下载的图片
+		try{
+			 out = response.getOutputStream();
+			 Article art = articleManager.get(entity.getArticleId());
+			 if(null != art)
+			 {
+				 entity = imageManager.get(entity.getId());
+				 //TODO 设置图片类型
+			 	String url = entity.getImgUrl();
+				if (url.toLowerCase().endsWith(".gif")) {
+					response.setContentType("image/gif;");
+				} else if (url.toLowerCase().endsWith(".png")) {
+					response.setContentType("image/png;");
+				} else if (url.toLowerCase().endsWith(".jpg")){
+					response.setContentType("image/jpg;");
+				}else{
+					response.setContentType("image/jpg;");
+				}
+				byte[] body = HttpClientUtils.getResponseBodyAsByte(art.getArticleUrl(), null, url);
+				if(null != body && body.length > 0)
+				{
+					response.setContentLength(body==null?0:body.length);
+					out.write(body);
+					out.flush();
+				}
+			 }
+			 Thread.sleep(1500);
+			 //TODO 图片类型
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			if(null != out){
+				out.close();
+			}
+		}
+		return;
 	}
 	
 	public JMagickScale getTp() {
