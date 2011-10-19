92c7f99698e3a058c7ae978b051bfa6bfd6085f8
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-10-19 02:07:56 +0000

    2011-10-19 修改工具类中的方法。

--- trunk/ssi-common/src/com/ssi/common/utils/HttpClientUtils.java
+++ trunk/ssi-common/src/com/ssi/common/utils/HttpClientUtils.java
@@ -1,10 +1,15 @@
 package com.ssi.common.utils;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.net.URLEncoder;
 import java.net.UnknownHostException;
 import java.util.HashMap;
+import java.util.Iterator;
 
 import org.apache.commons.httpclient.DefaultHttpMethodRetryHandler;
 import org.apache.commons.httpclient.Header;
@@ -28,6 +33,19 @@ public class HttpClientUtils {
 
 	private static PostMethod postMethod = null;
 
+	/**
+	 * 根据HTTP的URL地址，获取文件输入流
+	 * 
+	 * @param httpURL
+	 * @return
+	 * @throws Exception
+	 */
+	public synchronized static InputStream getInputStreamByURL(String httpURL) throws Exception {
+		URL url = new URL(httpURL);
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		return connection.getInputStream();
+	}
+	
 	public static boolean validationURL(String url) {
 		boolean success = true;
 		try {
@@ -41,11 +59,9 @@ public class HttpClientUtils {
 				success = false;
 			}
 		} catch (IOException ioe) {
-			// System.err.println("HttpClient.validationURL.IOException:"+ioe.getMessage());
 			success = false;
 			return false;
 		} catch (Exception e) {
-			// System.err.println("HttpClient.validationURL.Exception:"+e.getMessage());
 			success = false;
 			return false;
 		} finally {
@@ -68,7 +84,7 @@ public class HttpClientUtils {
 	 * 2010-11-08
 	 *  修改对URI中地址中的空格的处理，将空格转化为%20
 	 */
-	public static String encodeURL(String url, String encode)
+	public synchronized static String encodeURL(String url, String encode)
 			throws UnsupportedEncodingException {
 		StringBuilder sb = new StringBuilder();
 		StringBuilder noAsciiPart = new StringBuilder();
@@ -97,18 +113,6 @@ public class HttpClientUtils {
 					case 44:
 						sb.append("%2C");
 						break;
-//					case 45:
-//						sb.append("%2D");
-//						break;
-//					case 46:
-//						sb.append("%2E");
-//						break;
-//					case 47:
-//						sb.append("%2F");
-//						break;
-//					case 58:
-//						sb.append("%3A");
-//						break;
 					case 59:
 						sb.append("%3B");
 						break;
@@ -136,12 +140,6 @@ public class HttpClientUtils {
 					case 93:
 						sb.append("%5D");
 						break;
-//					case 94:
-//						sb.append("%5E");
-//						break;
-//					case 95:
-//						sb.append("%5F");
-//						break;
 					case 96:
 						sb.append("%60");
 						break;
@@ -151,11 +149,10 @@ public class HttpClientUtils {
 				}
 			}
 		}
-		System.out.println(">> url:"+sb.toString());
 		return sb.toString();
 	}
 
-	public static boolean urlValidation(String url) {
+	public synchronized static boolean urlValidation(String url) {
 		try {
 			httpclient = new HttpClient();
 			getMethod = new GetMethod(encodeURL(url,"UTF-8"));
@@ -165,7 +162,6 @@ public class HttpClientUtils {
 			// 执行getMethod
 			int statusCode = httpclient.executeMethod(getMethod);
 			if (statusCode != HttpStatus.SC_OK) {
-				System.err.println("地址[" + url + "],状态码[" + statusCode + "]");
 				return false;
 			}
 		} catch (Exception e) {
@@ -179,51 +175,20 @@ public class HttpClientUtils {
 		return true;
 	}
 
-	static void test1(String url) {
-		long start = System.currentTimeMillis();
-		try {
-			httpclient = new HttpClient();
-			getMethod = new GetMethod(encodeURL(url,"UTF-8"));
-			// getMethod.getParams().setParameter(HttpMethodParams.RETRY_HANDLER,new
-			// DefaultHttpMethodRetryHandler());
-			// 执行getMethod
-			int statusCode = httpclient.executeMethod(getMethod);
-			System.out.println("URL:" + url + ",状态码:" + statusCode);
-			if (statusCode != HttpStatus.SC_OK) {
-				// System.err.println("Method failed: "+
-				// getMethod.getStatusLine());
-				// System.err.println("URL: "+ url);
-				// System.out.println();
-			}
-			if (null != getMethod)
-				getMethod.releaseConnection();
-			if (null != httpclient)
-				httpclient = null;
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			if (null != getMethod)
-				getMethod.releaseConnection();
-			if (null != httpclient)
-				httpclient = null;
-		}
-		long end = System.currentTimeMillis();
-		System.out.println("耗时:" + (end - start));
-	}
-
 	/**
 	 * 获取响应头
 	 * 
 	 * @param url
 	 * @return
 	 */
-	static HashMap<String, String> getHttpHeaderResponse(String url) {
+	public synchronized static HashMap<String, String> getHttpHeaderResponse(String url) {
 		HashMap<String, String> result = new HashMap<String, String>();
 		try {
 			httpclient = new HttpClient();
 			getMethod = new GetMethod(encodeURL(url,"UTF-8"));
 
 			int statusCode = httpclient.executeMethod(getMethod);
+			result.put("Response-Status", String.valueOf(statusCode));
 			if (statusCode == HttpStatus.SC_OK) {
 				Header[] headers = getMethod.getResponseHeaders();
 				for (Header header : headers) {
@@ -243,13 +208,46 @@ public class HttpClientUtils {
 	}
 
 	/**
+	 * 获取响应头
+	 * 
+	 * @param url
+	 * @return
+	 */
+	public synchronized static HashMap<String, Object> getResponse(String url) {
+		HashMap<String, Object> result = new HashMap<String, Object>();
+		try {
+			httpclient = new HttpClient();
+			getMethod = new GetMethod(encodeURL(url,"UTF-8"));
+
+			int statusCode = httpclient.executeMethod(getMethod);
+			result.put("Response-Status", String.valueOf(statusCode));
+			if (statusCode == HttpStatus.SC_OK) {
+				Header[] headers = getMethod.getResponseHeaders();
+				for (Header header : headers) {
+					// System.out.println(header.getName()+":"+header.getValue());
+					result.put(header.getName(), header.getValue());
+				}
+				result.put("body", getMethod.getResponseBody());
+			}
+		} catch (Exception e) {
+			System.err.println(e);
+		} finally {
+			if (null != getMethod)
+				getMethod.releaseConnection();
+			if (null != httpclient)
+				httpclient = null;
+		}
+		return result;
+	}
+	
+	/**
 	 * 从url中获取响应头的内容
 	 * 
 	 * @param url
 	 * @param headerName
 	 * @return
 	 */
-	public static String getHttpHeaderResponse(String url, String headerName) {
+	public synchronized static String getHttpHeaderResponse(String url, String headerName) {
 		String result = null;
 		try {
 			httpclient = new HttpClient();
@@ -276,7 +274,7 @@ public class HttpClientUtils {
 	 * @param url
 	 * @return
 	 */
-	public static String getResponseBody(String url) {
+	public synchronized static String getResponseBody(String url) {
 		String value = "";
 		try {
 			httpclient = new HttpClient();
@@ -329,7 +327,7 @@ public class HttpClientUtils {
 	 * @param url
 	 * @return
 	 */
-	public static int getResponseCode(String url){
+	public synchronized static int getResponseCode(String url){
 		int statusCode = -200;
 		try{
 			httpclient = new HttpClient();
@@ -356,10 +354,9 @@ public class HttpClientUtils {
 	 * @param url
 	 * @return
 	 */
-	public static byte[] getResponseBodyAsByte(String refence, String cookie,
+	public synchronized static byte[] getResponseBodyAsByte(String refence, String cookie,
 			String url) {
 		byte[] value = null;
-		String contentType = null;
 		try {
 			httpclient = new HttpClient();
 			getMethod = new GetMethod(encodeURL(url,"UTF-8"));
@@ -370,10 +367,8 @@ public class HttpClientUtils {
 				getMethod.setRequestHeader("Cookie", cookie);
 			int statusCode = httpclient.executeMethod(getMethod);
 			if (statusCode == HttpStatus.SC_OK) {
-				contentType = getMethod.getResponseHeader("Content-Type").getValue();
 				value = getMethod.getResponseBody();
 			}
-			System.out.println("contentType:"+contentType);
 		} catch (Exception e) {
 			System.err.println(e);
 		} finally {
@@ -417,48 +412,29 @@ public class HttpClientUtils {
 	}
 
 	public static void main(String args[]) {
-
 		try {
-			// byte[] value2 = getResponseBodyAsByte(null,
-			// null,"http://www.bizhizhan.com/uploads/allimg/090830/1-0ZS0102521.jpg");
-			// if(null != value2){
-			// System.out.println("未增加破解防盗链引用文件长度:"+value2.length);
-			// }
-			//			
-			// long start = System.currentTimeMillis();
-			// byte[] value =
-			// getResponseBodyAsByte("http://www.bizhizhan.com//renwenjijiabizhi/shoujitupian-30-4598.html",
-			// "rtime=2; ltime=1282990509523;
-			// cnzz_eid=5808015-1282816593-http%3A//www.tuku.cn/;
-			// virtualwall=vsid=0c8cafa6001de309645c11edffa3aa43",
-			// "http://www.bizhizhan.com/uploads/allimg/090830/1-0ZS0102521.jpg");
-			// if(null != value){
-			// long end = System.currentTimeMillis();
-			// System.out.println("增加破解防盗链引用后的文件长度:"+value.length);
-			// System.out.println("耗时:"+(end-start));
-			// IOUtil.createFile(value,
-			// System.getProperty("user.dir")+"/"+"1-0ZS0102521.jpg");
-			// }
-
-			// http://www.china.com/zh_cn/ 长度[2010-09-06 18:52]：173574
-//			String length = getHttpHeaderResponse(
-//					"http://www.china.com/zh_cn/", "Content-Length");
-//			System.out.println("网页长度：" + length);
-//			String uri= "http://www.showimg.com/star/my1200994724/big/张学友/Famke_Janssen _22780685.jpg";
-//			try{
-//				System.out.println(encodeURL(uri,"UTF-8"));
-//			}catch(Exception e){
-//				e.printStackTrace();
-//			}
-
-			// System.out.println("isTRUE:"+urlValidation("http://www.bizhi.com/wallpaper/1150_2.html"));
-			
-			
-//			String url = "http://www.showimg.com/nature/nature20100914/big/02833_034_ZOabx1li[1].jpg";
-//			byte[] big = getResponseBodyAsByte(null,null,url);
-//			System.out.println("长度:"+big.length);
-			int statusCode = getResponseCode("http://www.163.com/");
-			System.out.println(" >> 站点状态："+statusCode);
+			HashMap<String,Object> map = getResponse("http://www.china.com");
+			Iterator it = map.keySet().iterator();
+			while(it.hasNext()){
+				String key = (String)it.next();
+				Object value = map.get(key);
+				if(value instanceof String){
+					value = (String)value;
+				}
+				if(value instanceof byte[]){
+					int status = FileUtils.saveFile("d:"+File.separator+FileUtils.generateFileName("index.html"), (byte[])value, false);
+					System.out.println(" > status:"+status);
+				}
+//				int pos = key.lastIndexOf("-");
+//				if(pos != -1){
+//					String prex = key.substring(0,pos).toLowerCase();
+//					String ends = key.substring(pos+1);
+//					key = prex + ends;
+//				}else{
+//					key = key.toLowerCase();
+//				}
+				System.out.println(" > key["+key+"] | value["+value+"]");
+			}
 		} catch (Exception e) {
 			System.err.println(e);
 		} finally {
