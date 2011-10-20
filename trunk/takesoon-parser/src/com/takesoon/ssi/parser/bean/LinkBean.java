07660548d8de25ceff8d88d06f2af273cc3310fd
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-10-20 09:47:34 +0000

    2011-10-20 新增链接对象。

---
+++ trunk/takesoon-parser/src/com/takesoon/ssi/parser/bean/LinkBean.java
@@ -0,0 +1,69 @@
+package com.takesoon.ssi.parser.bean;
+
+public class LinkBean {
+	private String link;
+	private String name = "linkName";
+	private String title = "linkTitle";
+	private String attri = "linkAttri";
+	
+	public LinkBean() {
+	}
+	
+	public LinkBean(String link){
+		this.link = link;
+	}
+	
+	public LinkBean(String link,String name){
+		this.name = name;
+		this.link = link;
+	}
+	
+	public LinkBean(String link,String name,String title){
+		this.link = link;
+		this.name = name;
+		this.title = title;
+	}
+	
+	public LinkBean(String link,String name,String title,String attri){
+		this.link = link;
+		this.name = name;
+		this.title = title;
+		this.attri = attri;
+		
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAttri() {
+		return attri;
+	}
+
+	public void setAttri(String attri) {
+		this.attri = attri;
+	}
+	
+	
+	
+	
+}
