7b871adf1b4b2f37bd129414eef34c18fa33a57d
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-10-20 09:48:11 +0000

    2011-10-20 新增对网页的解析处理(初级)

--- trunk/takesoon-parser/src/com/takesoon/ssi/parser/china/military/IndexParser.java
+++ trunk/takesoon-parser/src/com/takesoon/ssi/parser/china/military/IndexParser.java
@@ -1,6 +1,7 @@
 package com.takesoon.ssi.parser.china.military;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
@@ -20,6 +21,7 @@ import com.takesoon.oms.ssi.entity.Website;
 import com.takesoon.oms.ssi.service.ArticleManager;
 import com.takesoon.oms.ssi.service.ImageManager;
 import com.takesoon.oms.ssi.service.WebsiteManager;
+import com.takesoon.ssi.parser.bean.LinkBean;
 
 @Component
 @Transactional
@@ -38,17 +40,29 @@ public class IndexParser {
 	@Autowired
 	private ImageManager imageManager;
 	
+	HashMap<String, LinkBean> LINKHASH = new HashMap<String, LinkBean>();
+	
 	public void start(){
 		if(null != websiteManager){
 			List<Website> list = websiteManager.findByParsentId(D_PARENT_ID);
 			for(Website ws:list){
 				// 单独处理子项逻辑
 				try {
-					logger.debug(" > webName:"+ws.getName());
+//					logger.debug(" > webName:"+ws.getName());
 					doProcess(ws);
 				} catch (Exception e) {
 					logger.error("Exception:{1}",e);
 				}
+			}
+			Iterator it = LINKHASH.keySet().iterator();
+			while (it.hasNext()) {
+				String key = (String) it.next();
+				LinkBean link = (LinkBean) LINKHASH.get(key);
+				logger.debug(link.getName()+"|"+link.getLink());
+			}
+			logger.debug(" > size:"+LINKHASH.size());
+			if(LINKHASH.size() > 0){
+				LINKHASH.clear();
 			}
 		}
 	}
@@ -67,13 +81,31 @@ public class IndexParser {
 		Parser p1 = new Parser();
 		p1.setURL(url);
 		p1.setEncoding("UTF-8");
-
+		
 		NodeFilter filter = new NodeClassFilter(LinkTag.class);
 		NodeList list = p1.extractAllNodesThatMatch(filter);
 		if (list != null && list.size() > 0) {
+			LinkBean bean = null;
 			for (int i = 0; i < list.size(); i++) {
 				LinkTag link = (LinkTag) list.elementAt(i);
-				logger.debug(link.getLinkText() + "\t" + link.getLink());
+				
+				bean = new LinkBean();
+				bean.setLink(link.getLink());
+				String name = link.getAttribute("title") == null ? (link
+								.getLinkText() == null ? "无话题" : link
+								.getLinkText())
+								: link.getAttribute("title");
+				if (name.indexOf("“") != -1 || name.indexOf("”") != -1) {
+					name = name.replaceAll("“", "");
+					name = name.replace("”", "");
+				}
+				// 判断连接中是否存在创建文件夹时的非法字符
+				if (name.indexOf("\"") != -1 && name.indexOf("\"") != -1) {
+					name = name.replace("\"", "");
+				}
+
+				bean.setName(name);
+				LINKHASH.put(bean.getLink(), bean);
 			}
 		}
 		p1 = null;
