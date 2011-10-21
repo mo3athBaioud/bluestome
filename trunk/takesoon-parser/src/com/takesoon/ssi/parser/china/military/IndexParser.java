586f543e5d117827c7d2acc4bd76f9e773c25110
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-10-21 10:17:24 +0000

    2011-10-21 修改解析类。

--- trunk/takesoon-parser/src/com/takesoon/ssi/parser/china/military/IndexParser.java
+++ trunk/takesoon-parser/src/com/takesoon/ssi/parser/china/military/IndexParser.java
@@ -1,5 +1,7 @@
 package com.takesoon.ssi.parser.china.military;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -17,8 +19,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ssi.common.utils.FileUtils;
+import com.ssi.common.utils.HttpClientUtils;
+import com.ssi.common.utils.MD5;
 import com.takesoon.oms.ssi.entity.Website;
 import com.takesoon.oms.ssi.service.ArticleManager;
+import com.takesoon.oms.ssi.service.FileCacheManager;
 import com.takesoon.oms.ssi.service.ImageManager;
 import com.takesoon.oms.ssi.service.WebsiteManager;
 import com.takesoon.ssi.parser.bean.LinkBean;
@@ -40,7 +46,12 @@ public class IndexParser {
 	@Autowired
 	private ImageManager imageManager;
 	
+	@Autowired
+	private FileCacheManager fileCacheManager;
+	
 	HashMap<String, LinkBean> LINKHASH = new HashMap<String, LinkBean>();
+	
+	static Long SIZE_COUNT = 0l;
 	
 	public void start(){
 		if(null != websiteManager){
@@ -58,19 +69,97 @@ public class IndexParser {
 			while (it.hasNext()) {
 				String key = (String) it.next();
 				LinkBean link = (LinkBean) LINKHASH.get(key);
-				logger.debug(link.getName()+"|"+link.getLink());
+//				logger.debug(link.getName()+"|"+link.getLink());
+				try{
+					byte[] body = HttpClientUtils.getResponseBodyAsByte(link.getLink());
+					if(null != body && body.length > 0){
+						String md5 = MD5.getInstance().getMD5ofStr(body);
+						logger.debug(link.getName()+".md5:{1}"+md5);
+					}
+				}catch(Exception e){
+					logger.error(link.getLink()+" Exception:"+e.getMessage());
+					continue;
+				}
 			}
 			logger.debug(" > size:"+LINKHASH.size());
 			if(LINKHASH.size() > 0){
 				LINKHASH.clear();
 			}
 		}
+		
+	}
+	
+	/**
+	 * 测试往缓存填充文件
+	 *
+	 */
+	public void loopFillFile2Ehcache()
+	{
+		String filePath = "d:/test2.jpg";
+		byte[] body = null;
+		try 
+		{
+			body = FileUtils.readFileToByteArray(new File(filePath));
+			for(int i=0;i<1242;i++)
+			{
+				if(null != body && body.length > 0)
+				{
+					SIZE_COUNT += body.length;
+					String key = String.valueOf(System.currentTimeMillis());
+//					fileCacheManager.putByte(key, body);
+					logger.debug("i["+i+"] put key["+key+"]");
+					Thread.sleep(10);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		logger.debug(" > fill size:" + SIZE_COUNT);
 	}
 
 	private void doProcess(Website bean) throws Exception {
 		logger.debug(" >> url.id[" + bean.getId() + "] url.url["+ bean.getUrl() + "]");
 		try {
-			getLink(bean.getUrl());
+			//先根据URL取出md5，再根据md5取出byte[]，比较md5是否一致，如果一致的话，就不处理
+			String md5 = fileCacheManager.getUrl(bean.getUrl());
+			if(null != md5)
+			{
+				//TODO 可以将URL对应的文件下载到本地，然后在本地执行解析
+				byte[] body = HttpClientUtils.getResponseBodyAsByte(bean.getUrl());
+				if(null != body && body.length > 0)
+				{
+					String tmd5 = MD5.getInstance().getMD5ofStr(body);
+					if(!md5.equals(tmd5)){
+						logger.debug(" > md5 different ,will  remove old one of then!");
+						fileCacheManager.removeByte(bean.getUrl());
+						fileCacheManager.removeByte(md5);
+						
+						fileCacheManager.putUrl(bean.getUrl(), tmd5);
+						fileCacheManager.putByte(tmd5, body);
+//						String html = new String(body,"UTF-8");
+//						getLinkByHtml(html);
+					}
+				}
+			}
+			else
+			{
+				logger.debug(" > md5 is null,try to get new one!");
+				//TODO 可以将URL对应的文件下载到本地，然后在本地执行解析
+				byte[] body = HttpClientUtils.getResponseBodyAsByte(bean.getUrl());
+				if(null != body && body.length > 0)
+				{
+					String tmd5 = MD5.getInstance().getMD5ofStr(body);
+					fileCacheManager.putUrl(bean.getUrl(), tmd5);
+					fileCacheManager.putByte(tmd5, body);
+//					String html = new String(body,"UTF-8");
+//					getLinkByHtml(html);
+				}
+				
+			}
 		} catch (Exception e) {
 			logger.error("Exception:{1}",e);
 		}
@@ -91,10 +180,7 @@ public class IndexParser {
 				
 				bean = new LinkBean();
 				bean.setLink(link.getLink());
-				String name = link.getAttribute("title") == null ? (link
-								.getLinkText() == null ? "无话题" : link
-								.getLinkText())
-								: link.getAttribute("title");
+				String name = link.getLinkText();
 				if (name.indexOf("“") != -1 || name.indexOf("”") != -1) {
 					name = name.replaceAll("“", "");
 					name = name.replace("”", "");
@@ -111,4 +197,34 @@ public class IndexParser {
 		p1 = null;
 	}
 	
+	void getLinkByHtml(String html) throws Exception {
+		Parser p1 = new Parser();
+		p1.setInputHTML(html);
+		p1.setEncoding("UTF-8");
+		
+		NodeFilter filter = new NodeClassFilter(LinkTag.class);
+		NodeList list = p1.extractAllNodesThatMatch(filter);
+		if (list != null && list.size() > 0) {
+			LinkBean bean = null;
+			for (int i = 0; i < list.size(); i++) {
+				LinkTag link = (LinkTag) list.elementAt(i);
+				
+				bean = new LinkBean();
+				bean.setLink(link.getLink());
+				String name = link.getLinkText();
+				if (name.indexOf("“") != -1 || name.indexOf("”") != -1) {
+					name = name.replaceAll("“", "");
+					name = name.replace("”", "");
+				}
+				// 判断连接中是否存在创建文件夹时的非法字符
+				if (name.indexOf("\"") != -1 && name.indexOf("\"") != -1) {
+					name = name.replace("\"", "");
+				}
+
+				bean.setName(name);
+				LINKHASH.put(bean.getLink(), bean);
+			}
+		}
+		p1 = null;
+	}
 }
