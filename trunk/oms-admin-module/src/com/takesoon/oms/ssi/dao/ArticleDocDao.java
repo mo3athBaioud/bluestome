aa897ec056de164a694e701aabc63a19f5be0256
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-11-03 13:12:05 +0000

    2011-11-03 修改缓存操作，以及对dao操作记录操作时间的代码。

--- trunk/oms-admin-module/src/com/takesoon/oms/ssi/dao/ArticleDocDao.java
+++ trunk/oms-admin-module/src/com/takesoon/oms/ssi/dao/ArticleDocDao.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.hibernate.Hibernate;
 import org.hibernate.HibernateException;
 import org.hibernate.Query;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.skymobi.webframework.ssh3.core.orm.hibernate.CURDHibernateDao;
@@ -15,10 +17,15 @@ import com.takesoon.oms.ssi.entity.ArticleDoc;
 @Component
 public class ArticleDocDao extends CURDHibernateDao<ArticleDoc,Integer> {
 	
+	private static Logger logger = LoggerFactory.getLogger(ArticleDocDao.class);
+	
 	public int getTotalBySQL(String sql) throws HibernateException{
+		long start = System.currentTimeMillis();
 		int c = 0;
 		Query query = getSession().createSQLQuery(sql).addScalar("total", Hibernate.INTEGER);
 		c = (Integer)query.uniqueResult();
+		long end = System.currentTimeMillis();
+		System.out.println("> ArticleDocDao.getTotalBySQL 耗时:{"+(end-start)+"} ms");
 		return c;
 	}
 
@@ -29,10 +36,13 @@ public class ArticleDocDao extends CURDHibernateDao<ArticleDoc,Integer> {
 	 * @throws HibernateException
 	 */
 	public List<ArticleDoc> getListBySQL(String sql) throws HibernateException{
+		long start = System.currentTimeMillis();
 		List<ArticleDoc> list = new ArrayList<ArticleDoc>();
 		int c = 0;
 		Query query = getSession().createSQLQuery(sql).addEntity(ArticleDoc.class);
 		list = query.list();
+		long end = System.currentTimeMillis();
+		System.out.println("> ArticleDocDao.getListBySQL 耗时:{"+(end-start)+"} ms");
 		return list;
 	}
 
