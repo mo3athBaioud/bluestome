3faeae732f974d6cddccf98817babf67dbb3d9b2
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-11-24 07:24:34 +0000

    2011-11-24 修改DAO方法。

--- trunk/xian/xian-model/src/main/java/com/xian/support/dao/BDistrictDao.java
+++ trunk/xian/xian-model/src/main/java/com/xian/support/dao/BDistrictDao.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Component;
 import com.xian.support.entity.BDistrict;
 
 @Component
-public class BDistrictDao extends CURDHibernateDao<BDistrict,Integer> {
+public class BDistrictDao extends CURDHibernateDao<BDistrict,String> {
 
 }
