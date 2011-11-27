1c9f524140b40a72d308880a14611a7b747ae005
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-11-27 14:04:24 +0000

    2011-11-27 新增业务逻辑测试类。

---
+++ xian/xian-model/src/test/java/test/com/xian/support/manager/BussinessSimplifylManagerTest.java
@@ -0,0 +1,55 @@
+package test.com.xian.support.manager;
+
+import java.util.List;
+
+import org.apache.xbean.spring.context.ClassPathXmlApplicationContext;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+
+import com.xian.support.entity.BussinessSimplify;
+import com.xian.support.entity.Channel;
+import com.xian.support.service.BussinessSimplifyManager;
+import com.xian.support.service.StaffManager;
+
+public class BussinessSimplifylManagerTest {
+
+	private BussinessSimplifyManager bussinessSimplifyManager;
+	
+	@Before
+	public void init(){
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		bussinessSimplifyManager = (BussinessSimplifyManager)context.getBean("bussinessSimplifyManager");
+	}
+	
+	@After
+	public void destory(){
+		if(null != bussinessSimplifyManager)
+			bussinessSimplifyManager = null;
+	}
+	
+	@Test
+	public void test(){
+		BussinessSimplify entity = new BussinessSimplify();
+		if(null != bussinessSimplifyManager)
+		{
+			String bdistrict = "E0ED";
+			entity.setBdistrict(bdistrict);
+			int c = bussinessSimplifyManager.getTotalBySql(entity);
+			if(c > 0){
+				List<BussinessSimplify> list = bussinessSimplifyManager.getListBySql(entity, 0, 100);
+				if(null != list && list.size() > 0){
+					for(BussinessSimplify bs:list){
+						System.out.println(" > bs.id:"+bs.getId());
+						System.out.println(" > bs.btype:"+bs.getBtype());
+						System.out.println(" > bs.phonenum:"+bs.getPhonenum());
+						System.out.println();
+					}
+				}
+			}
+			
+		}
+	}
+	
+}
