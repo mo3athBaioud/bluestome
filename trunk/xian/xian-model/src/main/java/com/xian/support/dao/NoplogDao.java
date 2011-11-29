e8f08935ca0046c40dee39b585a789bc53c53439
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-11-29 12:52:38 +0000

    2011-11-29 修改日志DAO类。

--- trunk/xian/xian-model/src/main/java/com/xian/support/dao/NoplogDao.java
+++ trunk/xian/xian-model/src/main/java/com/xian/support/dao/NoplogDao.java
@@ -2,7 +2,9 @@ package com.xian.support.dao;
 
 import org.springframework.stereotype.Component;
 
+import com.xian.support.entity.Noplog;
+
 @Component
-public class NoplogDao extends CURDHibernateDao<NoplogDao,Integer> {
+public class NoplogDao extends CURDHibernateDao<Noplog,Integer> {
 
 }
