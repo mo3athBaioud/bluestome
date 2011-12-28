32db9dedee4c03ac61c8f79772bfcefafb8d85bd
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2011-12-28 01:25:26 +0000

    2011-12-28 新增相关网络操作方法

--- trunk/diy_page/src/com/utils/HttpClientUtils.java
+++ trunk/diy_page/src/com/utils/HttpClientUtils.java
@@ -1,7 +1,10 @@
 package com.utils;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.net.UnknownHostException;
+import java.util.Date;
 import java.util.HashMap;
 
 import org.apache.commons.httpclient.DefaultHttpMethodRetryHandler;
@@ -10,7 +13,10 @@ import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.params.HttpClientParams;
 import org.apache.commons.httpclient.params.HttpMethodParams;
+
+import com.chinamilitary.util.DateUtils;
 
 public class HttpClientUtils {
 
@@ -19,6 +25,9 @@ public class HttpClientUtils {
 	private static GetMethod getMethod = null;
 
 	private static PostMethod postMethod = null;
+	
+	//HTTP响应头中的文件大小描述
+	public static String CONTENTLENGTH = "Content-Length";
 
 	/**
 	 * 验证URL是否可用
@@ -91,6 +100,7 @@ public class HttpClientUtils {
 	 * @return
 	 */
 	public static String getHttpHeaderResponse(String url, String headerName) {
+		System.out.println("in");
 		String result = null;
 		long start = System.currentTimeMillis();
 		try {
@@ -141,17 +151,133 @@ public class HttpClientUtils {
 		return value;
 	}
 
+	/**
+	 * 获取页面的最后修改时间
+	 * @param url
+	 * @return
+	 */
+	public static String getLastModifiedByUrl(String url){
+		String value = null;
+		try{
+			String time = getHttpHeaderResponse(url, "Last-Modified");
+			Date date = DateUtils.parserDate(time);
+			value = DateUtils.formatDate(date, DateUtils.FULL_STANDARD_PATTERN2);
+		}catch(Exception e){
+			e.printStackTrace();
+		}		
+		return value;
+	}
+	
+	/**
+	 * 获取响应体
+	 * 
+	 * @param url
+	 *            实际下载地址
+	 * @return
+	 */
+	public static byte[] getResponseBodyAsByte(String url) {
+		byte[] value = null;
+		value = getResponseBodyAsByte(null, null, url);
+		return value;
+	}
+
+	/**
+	 * 获取响应体
+	 * 
+	 * @param cookie
+	 *            cookie内容
+	 * @param url
+	 *            实际下载地址
+	 * @return
+	 */
+	public static byte[] getResponseBodyAsByte(String cookie, String url) {
+		byte[] value = null;
+		value = getResponseBodyAsByte(null, cookie, url);
+		return value;
+	}
+
+	public static String encodeURL(String url, String encode) throws UnsupportedEncodingException {
+		StringBuilder sb = new StringBuilder();
+		StringBuilder noAsciiPart = new StringBuilder();
+		for (int i = 0; i < url.length(); i++) {
+			char c = url.charAt(i);
+			if (c > 255) {
+				noAsciiPart.append(c);
+			} else {
+				if (noAsciiPart.length() != 0) {
+					sb
+							.append(URLEncoder.encode(noAsciiPart.toString(),
+									encode));
+					noAsciiPart.delete(0, noAsciiPart.length());
+				}
+				sb.append(c);
+			}
+		}
+		return sb.toString();
+	}
+
+	/**
+	 * 获取响应体
+	 * 
+	 * @param refence
+	 *            引用的地址
+	 * @param cookie
+	 *            页面保存的信息
+	 * @param url
+	 * @return
+	 */
+	public static byte[] getResponseBodyAsByte(String refence, String cookie,
+			String url) {
+		byte[] value = null;
+		HttpClientParams params = null;
+		Header charset = null;
+		try {
+			httpclient = new HttpClient();
+			// params.setContentCharset("GBK");
+			getMethod = new GetMethod(encodeURL(url,"UTF-8"));
+			// getMethod.setParams(params);
+
+			// 破解防盗链配置
+			if (null != refence)
+				getMethod.addRequestHeader("Referer", refence);
+			if (null != cookie)
+				getMethod.setRequestHeader("Cookie", cookie);
+			int statusCode = httpclient.executeMethod(getMethod);
+			if (statusCode == HttpStatus.SC_OK) {
+//				charset = getMethod.getResponseHeader("charset");
+//				if(null ==  charset){
+//					
+//				}else{
+					value = getMethod.getResponseBody();
+//				}
+			}
+		} catch (Exception e) {
+			System.err.println(e);
+		} finally {
+			if (null != getMethod)
+				getMethod.releaseConnection();
+			if (null != httpclient)
+				httpclient = null;
+		}
+		return value;
+	}
+
 	public static void main(String args[]) {
 
 		String value = "";
 		String url = "http://tuku.military.china.com/military/html/2010-04-02/139456.xml";
 		String pushUrl = "http://localhost:8080/mrpmusicfs/mrpmusic.do";
+		String imgurl = "http://bizhi.zhuoku.com/2009//03/25/lvye/Lvye43.jpg";
 		try {
 			
-//			boolean suc = validationURL(url);
-			boolean suc = validationURL(pushUrl);
+			String length = getHttpHeaderResponse(imgurl,CONTENTLENGTH);
 			
-			System.out.println("suc:"+suc);
+			System.out.println(length == null ? "NULL" : length );
+			
+//			boolean suc = validationURL(url);
+//			boolean suc = validationURL(pushUrl);
+			
+//			System.out.println("suc:"+suc);
 			
 //			httpclient = new HttpClient();
 //			getMethod = new GetMethod(
