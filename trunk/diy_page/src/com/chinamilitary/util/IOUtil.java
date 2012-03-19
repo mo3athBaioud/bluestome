c52692fd6e6b5d1860726914a042ddc2cd3c51c9
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2012-03-19 08:56:00 +0000

    2012-03-19 1.修改createFile中对目录不存在的情况下，新建目录。 2.新增saveFile方法。

--- trunk/diy_page/src/com/chinamilitary/util/IOUtil.java
+++ trunk/diy_page/src/com/chinamilitary/util/IOUtil.java
@@ -121,6 +121,9 @@ public class IOUtil {
 		try{
 			try{
 				file = new File(fileName+ext);
+				if(!file.exists()){
+					file.getParentFile().mkdirs();
+				}
 				out = new BufferedOutputStream(new FileOutputStream(file),1024);
 				out.write(content.getBytes(), 0, content.getBytes().length);
 				out.close();
@@ -283,4 +286,36 @@ public class IOUtil {
 			e.printStackTrace();
 		}
 	}
+	
+	/**
+	 * 将字节数组保存在指定文件中
+	 * 
+	 * @param pathname
+	 *            需要保存的文件路径
+	 * @param buffer
+	 *            需要被保存的内容
+	 * @param append
+	 *            是否采用追加的方式
+	 * @return
+	 */
+	public static int saveFile(String pathname, byte[] buffer, boolean append) {
+		String path = null;
+		pathname = pathname.replaceAll("\\\\", "/");
+		path = pathname.substring(0, pathname.lastIndexOf("/"));
+		java.io.File dir = new java.io.File(path);
+		if (!dir.isDirectory())
+			dir.mkdirs();// 创建不存在的目录
+		try {
+			// writeln the file to the file specified
+			OutputStream bos = new FileOutputStream(pathname, append);
+			bos.write(buffer);
+			bos.close();
+		} catch (FileNotFoundException fnfe) {
+			return -1;
+		} catch (IOException ioe) {
+			return -2;
+		}
+		return 0;
+	}
+	
 }
