d500e6f20aacfcb7992a28a530e9ba1c24c5af7f
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2012-07-11 02:09:34 +0000

    2012-07-11 修改C类中，遇到有新帖时，播放声音来提示。

--- trunk/diy_page/src/com/autohome/C.java
+++ trunk/diy_page/src/com/autohome/C.java
@@ -1,10 +1,12 @@
 package com.autohome;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,6 +18,14 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 import org.htmlparser.NodeFilter;
 import org.htmlparser.Parser;
 import org.htmlparser.filters.HasAttributeFilter;
@@ -26,6 +36,7 @@ import org.htmlparser.util.NodeList;
 public class C {
 	
 	static String BSS_URL = "http://club.autohome.com.cn/bbs/forum-c-{id}-{page}.html";
+	static String BBS_POST_URL = "http://club.autohome.com.cn/bbs/thread-c-{cid}-{pid}-1.html";
 	BlockingQueue<byte[]> byteQuene = new LinkedBlockingQueue<byte[]>(100);
 
 	static Map<String, Integer> SIZEHASH = new HashMap<String, Integer>();
@@ -81,7 +92,6 @@ public class C {
 	}
 
 	public void executorPoll(final String carId,final String page) {
-		System.out.println("ThreadID:"+Thread.currentThread().getId());
 		final long pollTime = 30 * 1000L;
 		final long timeout = 500L;
 		final ScheduledExecutorService pool = Executors
@@ -107,6 +117,7 @@ public class C {
 													if(null == value){
 														SIZEHASH.put(key, 1);
 														String content = new String(body);
+														//TODO 解析页面内容
 														parserBbs(content,"2001");
 													}else{
 														SIZEHASH.put(key, (value+1));
@@ -132,6 +143,11 @@ public class C {
 		}, 100L, pollTime, TimeUnit.MILLISECONDS);
 	}
 	
+	/**
+	 * 解析页面内容
+	 * @param content
+	 * @param carId
+	 */
 	private void parserBbs(String content,String carId){
 		long start = System.currentTimeMillis();
 		Parser p1 = null;
@@ -143,7 +159,6 @@ public class C {
 			NodeList list = p1.extractAllNodesThatMatch(fileter)
 					.extractAllNodesThatMatch(
 							new HasAttributeFilter("class", "list_dl "));
-			System.out.println(" 列表数量:"+list.size());
 			/**
 			 * c|2001|16099453|3|2012-07-10 13:36|2012-07-10 20:02|5385276|1048731|0|0|0|tnwwzh 
 			 * 格式说明:
@@ -154,15 +169,25 @@ public class C {
 			 */
 			if(null != list && list.size() > 0){
 				int i =0;
+				int c = 0;
 				do{
 					CompositeTag tag = (CompositeTag)list.elementAt(i);
 					String lang = tag.getAttribute("lang");
 					String[] paras = lang.split("\\|");
-					if(paras.length > 0 && paras[1].trim().equals(carId)){
-						System.out.println(lang);
+					if(paras.length > 0 && paras[1].trim().equals(carId) && paras[3].trim().equals("0")){
+						//播放提示音
+						MediaPlayCase.play();
+						String pid = paras[2].trim();
+						String postURL = BBS_POST_URL.replace("{cid}", carId).replace("{pid}", pid);
+						System.out.println("\t>>>>>>新帖:["+lang+"]<<<<<<");
+						System.out.println("\t>>>>>>"+postURL+"<<<<<<");
+						c++;
 					}
 					i++;
 				}while(i<list.size());
+				System.out.println("帖子数:"+c);
+			}else{
+				System.err.println("获取列表失败");
 			}
 		}catch(Exception e){
 			e.printStackTrace();
@@ -170,12 +195,80 @@ public class C {
 			if(null != p1){
 				p1 = null;
 			}
-			System.out.println("耗时:"+(System.currentTimeMillis()-start)+"ms");
+			System.out.println("解析页面耗时:"+(System.currentTimeMillis()-start)+"ms");
 		}
 	}
 
 	public static void main(String args[]) {
 		C c = new C();
 		c.executorPoll("2001","1");
+	}
+}
+
+/**
+ * 播放音频文件
+ * @author Bluestome.Zhang
+ *
+ */
+class MediaPlayCase{
+	static void play() {
+		new Thread(new Runnable(){
+			public void run() {
+				try {
+					// From file
+					AudioInputStream stream = AudioSystem
+							.getAudioInputStream(new File(
+									"D:\\projects\\sky2.0\\qunke2\\diy_page\\src\\com\\autohome\\notify.wav"));
+
+					// From URL
+					// stream = AudioSystem.getAudioInputStream(new URL(
+					// "http://hostname/audiofile"));
+
+					// At present, ALAW and ULAW encodings must be converted
+					// to PCM_SIGNED before it can be played
+					AudioFormat format = stream.getFormat();
+					if (format.getEncoding() != AudioFormat.Encoding.PCM_SIGNED) {
+						format = new AudioFormat(
+								AudioFormat.Encoding.PCM_SIGNED, format
+										.getSampleRate(), format
+										.getSampleSizeInBits() * 2, format
+										.getChannels(),
+								format.getFrameSize() * 2, format
+										.getFrameRate(), true); // big endian
+						stream = AudioSystem
+								.getAudioInputStream(format, stream);
+					}
+
+					// Create the clip
+					DataLine.Info info = new DataLine.Info(Clip.class, stream
+							.getFormat(),
+							((int) stream.getFrameLength() * format
+									.getFrameSize()));
+					Clip clip = (Clip) AudioSystem.getLine(info);
+
+					// This method does not return until the audio file is
+					// completely loaded
+					clip.open(stream);
+
+					// Start playing
+					clip.start();
+					
+					Thread.sleep(2*1000L);
+					
+					clip.stop();
+					clip = null;
+				} catch (MalformedURLException e) {
+					System.err.println(e);
+				} catch (IOException e) {
+					System.err.println(e);
+				} catch (LineUnavailableException e) {
+					System.err.println(e);
+				} catch (UnsupportedAudioFileException e) {
+					System.err.println(e);
+				} catch (Exception e){
+					System.err.println(e);
+				}
+			}
+		}).start();
 	}
 }
