9c9bc77e7617f048b753bace92842b3b72d7fc89
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2012-09-21 04:58:22 +0000

    2012-09-21 修改下载Task。

--- trunk/android/AsyncTask/src/com/bluestome/asynctask/MainActivity.java
+++ trunk/android/AsyncTask/src/com/bluestome/asynctask/MainActivity.java
@@ -1,3 +1,4 @@
+
 package com.bluestome.asynctask;
 
 import android.app.Activity;
@@ -7,46 +8,47 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import com.bluestome.asynctask.task.DownloadTask;
 import com.bluestome.asynctask.task.UrlLoadTask;
 
 public class MainActivity extends Activity {
-        
+
     Button download;
     ProgressBar pb;
     ProgressBar pb2;
     TextView tv;
     TextView pbtv;
     TextView pbtv2;
-    
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        pb=(ProgressBar)findViewById(R.id.pb);
-        pb2=(ProgressBar)findViewById(R.id.pb2);
-        tv=(TextView)findViewById(R.id.tv);
-        pbtv=(TextView)findViewById(R.id.textView_pb);
-        pbtv2=(TextView)findViewById(R.id.textView_pb2);
-        
-        download = (Button)findViewById(R.id.download);
+        pb = (ProgressBar) findViewById(R.id.pb);
+        pb2 = (ProgressBar) findViewById(R.id.pb2);
+        tv = (TextView) findViewById(R.id.tv);
+        pbtv = (TextView) findViewById(R.id.textView_pb);
+        pbtv2 = (TextView) findViewById(R.id.textView_pb2);
+
+        download = (Button) findViewById(R.id.download);
         download.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 pb.setVisibility(View.VISIBLE);
                 pb2.setVisibility(View.VISIBLE);
-                //将按钮设置为不可用
+                // 将按钮设置为不可用
                 download.setEnabled(false);
-                
-                UrlLoadTask taks = new UrlLoadTask(MainActivity.this,download,pbtv,pb);
+
+                UrlLoadTask taks = new UrlLoadTask(MainActivity.this, download, pbtv, pb);
                 taks.execute("http://img1.gtimg.com/0/29/2969/296943_1200x1000_0.jpg");
-                
-                UrlLoadTask taks2 = new UrlLoadTask(MainActivity.this,download,pbtv2,pb2);
+
+                UrlLoadTask taks2 = new UrlLoadTask(MainActivity.this, download, pbtv2, pb2);
+                // http://211.136.90.66/mms/res/500k
                 taks2.execute("http://ww4.sinaimg.cn/bmiddle/47465aa5jw1dt7xuz6klyj.jpg");
+                // taks2.execute("http://221.179.7.248/CSSPortal/attach/Widget/upload/ProductShowHall/5235534f-578e-4389-a1a3-208e6f2f2dc1-2010-09-08-05-47-39.jpg?nocache=1348198990423");
+
             }
         });
-        
+
     }
-    
 }
