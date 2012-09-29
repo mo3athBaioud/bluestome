4efe370e30fe898d12cb067abde9c8787cb632ad
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2012-09-29 10:00:18 +0000

    2012-09-29 1.修正NetworkActivity中获取激活网络为空时抛出的空指针异常。 2.修改在广播中创建线程时，在线程调用UI空间时异常的错误。

--- trunk/android/DevInfoDemo/src/com/example/devinfodemo/broadcast/DeviceBootReceiver.java
+++ trunk/android/DevInfoDemo/src/com/example/devinfodemo/broadcast/DeviceBootReceiver.java
@@ -2,10 +2,13 @@
 package com.example.devinfodemo.broadcast;
 
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Handler;
 import android.os.Message;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -22,6 +25,7 @@ public class DeviceBootReceiver extends BroadcastReceiver {
     private static final String TAG = DeviceBootReceiver.class.getSimpleName();
     private static final int MSG_DELAY_START_SERVICE = 0x01;
     private static final int DELAY_MILLIS = 3000;// 3000ms
+    protected ContentResolver resolver;
 
     private final Handler mHandler = new Handler() {
         @Override
@@ -37,12 +41,51 @@ public class DeviceBootReceiver extends BroadcastReceiver {
     };
 
     @Override
-    public void onReceive(Context context, Intent intent) {
+    public void onReceive(final Context context, Intent intent) {
+
         if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
             Log.e(TAG, "DeviceBootReceiver broadcast is receiver");
             mHandler.sendMessageDelayed(mHandler.obtainMessage(MSG_DELAY_START_SERVICE, context),
                     DELAY_MILLIS);
             Toast.makeText(context, "启动完成...", Toast.LENGTH_LONG).show();
+            getContactsLocalList(context);
         }
     }
+
+    public void getContactsLocalList(Context context) {
+        int cc = 0;
+        int rcc = 0;
+
+        resolver = context.getContentResolver();
+        StringBuilder sb = new StringBuilder();
+
+        // contacts 表中的联系人
+        Cursor cursor = resolver.query(
+                ContactsContract.Contacts.CONTENT_URI,
+                new String[] {
+                    ContactsContract.Contacts._ID
+                }, null, null, null);
+
+        while (cursor.moveToNext()) {
+            cc = cursor.getCount();
+            break;
+        }
+        cursor.close();
+
+        // raw_contacts 表中的联系人
+        cursor = resolver.query(
+                ContactsContract.RawContacts.CONTENT_URI,
+                new String[] {
+                        ContactsContract.RawContacts._ID
+                }, null,
+                null, null);
+
+        sb = sb.delete(0, sb.length());
+        while (cursor.moveToNext()) {
+            rcc = cursor.getCount();
+        }
+        cursor.close();
+        Toast.makeText(context, "cc=" + cc + "|rcc" + rcc, Toast.LENGTH_LONG).show();
+    }
+
 }
