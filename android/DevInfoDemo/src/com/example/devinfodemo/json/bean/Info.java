428d095bcaed2bd6763069d91786cfc611881c8e
bluestomez <bluestomez@8d0e2b01-6282-6274-ba5d-974b8f8e99e7>
2012-10-09 07:56:43 +0000

    2012-10-09 修改AboutActivity中对电池电量广播未在界面退出时进行反注册。

---
+++ android/DevInfoDemo/src/com/example/devinfodemo/json/bean/Info.java
@@ -0,0 +1,28 @@
+
+package com.example.devinfodemo.json.bean;
+
+/**
+ * @ClassName: Info
+ * @Description: TODO
+ * @author Bluestome.Zhang
+ * @date 2012-10-9 下午03:43:21
+ */
+public class Info {
+
+    protected String exceptionId;
+
+    /**
+     * @return the exceptionId
+     */
+    public String getExceptionId() {
+        return exceptionId;
+    }
+
+    /**
+     * @param exceptionId the exceptionId to set
+     */
+    public void setExceptionId(String exceptionId) {
+        this.exceptionId = exceptionId;
+    }
+
+}
